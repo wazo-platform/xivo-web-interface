5cc2fba1eac41c245347cbf7030a51c3c145711b
Cedric Abunar <cabunar@avencall.com>
2012-08-10 09:35:37 -0400

    [fix]do not display cell if no config selected

--- src/tpl/www/bloc/statistics/call_center/data/stats1.php
+++ src/tpl/www/bloc/statistics/call_center/data/stats1.php
@@ -37,6 +37,11 @@ if (($type = $table1->get_data_custom('listtype')) !== null
 		<span class="span-right">&nbsp;</span>
 	</h3>
 	<div class="sb-content">
+<?php
+	if (($msg = $table1->get_error()) !== false):
+		echo $msg;
+	else :
+?>
 		<div class="sb-list">
 <?php
 		echo $table1->infos_html();
@@ -46,6 +51,7 @@ if (($type = $table1->get_data_custom('listtype')) !== null
 <?php
 		$xivo_jqplot->get_result('chart1');
 		$xivo_jqplot->get_result('chart2');
+	endif;
 ?>
 	</div>
 	<div class="sb-foot xspan">
