382e748d2713c35a29dc93cfde4c2a17f5a86bc4
Etienne Lessard <elessard@avencall.com>
2012-09-14 13:53:02 -0400

    move "enable call recording" near other "enable" options.

--- src/tpl/www/bloc/service/ipbx/asterisk/pbx_settings/users/form.php
+++ src/tpl/www/bloc/service/ipbx/asterisk/pbx_settings/users/form.php
@@ -475,17 +475,17 @@ endif;
 				      'default'	=> $element['userfeatures']['enablexfer']['default'],
 				      'checked'	=> $info['userfeatures']['enablexfer'])),
 
-		$form->checkbox(array('desc'	=> $this->bbf('fm_userfeatures_callrecord'),
-				      'name'	=> 'userfeatures[callrecord]',
-				      'labelid'	=> 'userfeatures-callrecord',
-				      'default'	=> $element['userfeatures']['callrecord']['default'],
-				      'checked'	=> $info['userfeatures']['callrecord'])),
-
 		$form->checkbox(array('desc'	=> $this->bbf('fm_userfeatures_enableautomon'),
 				      'name'	=> 'userfeatures[enableautomon]',
 				      'labelid'	=> 'userfeatures-enableautomon',
 				      'default'	=> $element['userfeatures']['enableautomon']['default'],
 				      'checked'	=> $info['userfeatures']['enableautomon'])),
+
+		$form->checkbox(array('desc'	=> $this->bbf('fm_userfeatures_callrecord'),
+				      'name'	=> 'userfeatures[callrecord]',
+				      'labelid'	=> 'userfeatures-callrecord',
+				      'default'	=> $element['userfeatures']['callrecord']['default'],
+				      'checked'	=> $info['userfeatures']['callrecord'])),
 
 		$form->checkbox(array('desc'	=> $this->bbf('fm_userfeatures_incallfilter'),
 				      'name'	=> 'userfeatures[incallfilter]',
