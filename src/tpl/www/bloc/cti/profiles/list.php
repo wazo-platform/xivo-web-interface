c122fe61865021798a7de5e5195d8388447a698a
Cedric Abunar <cabunar@avencall.com>
2012-11-23 13:47:35 -0500

    [evo]use new system of data pgsql for cti_profile

--- src/tpl/www/bloc/cti/profiles/list.php
+++ src/tpl/www/bloc/cti/profiles/list.php
@@ -1,8 +1,7 @@
 <?php
-
 #
 # XiVO Web-Interface
-# Copyright (C) 2006-2011  Avencall
+# Copyright (C) 2012  Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -26,35 +25,27 @@ $pager = $this->get_var('pager');
 $act = $this->get_var('act');
 
 $page = $url->pager($pager['pages'],
-		    $pager['page'],
-		    $pager['prev'],
-		    $pager['next'],
-		    'cti/profiles',
-		    array('act' => $act));
+			$pager['page'],
+			$pager['prev'],
+			$pager['next'],
+			'cti/profiles',
+			array('act' => $act));
 
 ?>
-<div id="sr-ctiprofiles" class="b-list">
+<div id="sr-cti_profile" class="b-list">
 <?php
 	if($page !== ''):
 		echo '<div class="b-page">',$page,'</div>';
 	endif;
 ?>
-<form action="#" name="fm-ctiprofiles-list" method="post" accept-charset="utf-8">
-<?php
-	echo	$form->hidden(array('name'	=> DWHO_SESS_NAME,
-				    'value'	=> DWHO_SESS_ID)),
-
-		$form->hidden(array('name'	=> 'act',
-				    'value'	=> $act)),
-
-		$form->hidden(array('name'	=> 'page',
-				    'value'	=> $pager['page']));
-?>
+<form action="#" name="fm-cti_profile-list" method="post" accept-charset="utf-8">
+<?=$form->hidden(array('name' => DWHO_SESS_NAME,'value' => DWHO_SESS_ID));?>
+<?=$form->hidden(array('name' => 'act',$act));?>
+<?=$form->hidden(array('name' => 'page','value' => $pager['page']));?>
 <table id="table-main-listing">
 	<tr class="sb-top">
 		<th class="th-left xspan"><span class="span-left">&nbsp;</span></th>
 		<th class="th-center"><?=$this->bbf('col_profile');?></th>
-		<th class="th-center"><?=$this->bbf('col_description');?></th>
 		<th class="th-center col-action"><?=$this->bbf('col_action');?></th>
 		<th class="th-right xspan"><span class="span-right">&nbsp;</span></th>
 	</tr>
@@ -62,7 +53,7 @@ $page = $url->pager($pager['pages'],
 	if(($list = $this->get_var('list')) === false || ($nb = count($list)) === 0):
 ?>
 	<tr class="sb-content">
-		<td colspan="5" class="td-single"><?=$this->bbf('no_profile');?></td>
+		<td colspan="4" class="td-single"><?=$this->bbf('no_profile');?></td>
 	</tr>
 <?php
 	else:
@@ -71,47 +62,44 @@ $page = $url->pager($pager['pages'],
 			$icon = 'enable';
 ?>
 	<tr onmouseover="this.tmp = this.className; this.className = 'sb-content l-infos-over';"
-	    onmouseout="this.className = this.tmp;"
-	    class="sb-content l-infos-<?=(($i % 2) + 1)?>on2">
+		onmouseout="this.className = this.tmp;"
+		class="sb-content l-infos-<?=(($i % 2) + 1)?>on2">
 		<td class="td-left">
 			<?=$form->checkbox(array('name'		=> 'profiles[]',
-						 'value'	=> $ref['ctiprofiles']['id'],
-						 'label'	=> false,
-						 'id'		=> 'it-profiles-'.$i,
-						 'checked'	=> false,
-						 'paragraph'	=> false));?>
+						'value'	=> $ref['id'],
+						'label'	=> false,
+						'id'		=> 'it-profiles-'.$i,
+						'checked'	=> false,
+						'paragraph'	=> false));?>
 		</td>
 		<td class="txt-left "
-		    title="<?=dwho_alttitle($ref['ctiprofiles']['name']);?>">
+			title="<?=dwho_alttitle($ref['name']);?>">
 <?php
-			echo	$url->img_html('img/site/flag/'.$icon.'.gif',null,'class="icons-list"');
-			echo $ref['ctiprofiles']['name'];
+			echo $url->img_html('img/site/flag/'.$icon.'.gif',null,'class="icons-list"');
+			echo $ref['name'];
 ?>
 		</td>
-		<td align="left"><?=$ref['ctiprofiles']['appliname']?></td>
 		<td class="td-right" colspan="2">
 <?php
 		echo	$url->href_html($url->img_html('img/site/button/edit.gif',
-						       $this->bbf('opt_modify'),
-						       'border="0"'),
-					'cti/profiles',
-					array('act'	=> 'edit',
-						   'idgroup'	=> $this->get_var('idgroup'),
-					      'idprofiles'	=> $ref['ctiprofiles']['id']),
-					null,
-					$this->bbf('opt_modify')),"\n";
+							$this->bbf('opt_modify'),
+							'border="0"'),
+							'cti/profiles',
+							array('act'	=> 'edit',
+							'id'	=> $ref['id']),
+							null,
+							$this->bbf('opt_modify')),"\n";
 
-		if($ref['ctiprofiles']['deletable']):
+		if($ref['deletable']):
 			echo	$url->href_html($url->img_html('img/site/button/delete.gif',
-							       $this->bbf('opt_delete'),
-							       'border="0"'),
-						'cti/profiles',
-						array('act'	=> 'delete',
-						      'idprofiles'	=> $ref['ctiprofiles']['id'],
-						      'idgroup'	=> $this->get_var('idgroup'),
-						      'page'	=> $pager['page']),
-						'onclick="return(confirm(\''.$dhtml->escape($this->bbf('opt_delete_confirm')).'\'));"',
-						$this->bbf('opt_delete'));
+							$this->bbf('opt_delete'),
+							'border="0"'),
+							'cti/profiles',
+							array('act'	=> 'delete',
+							'id'	=> $ref['id'],
+							'page'	=> $pager['page']),
+							'onclick="return(confirm(\''.$dhtml->escape($this->bbf('opt_delete_confirm')).'\'));"',
+							$this->bbf('opt_delete'));
 		endif;
 ?>
 		</td>
@@ -122,7 +110,7 @@ $page = $url->pager($pager['pages'],
 ?>
 	<tr class="sb-foot">
 		<td class="td-left xspan b-nosize"><span class="span-left b-nosize">&nbsp;</span></td>
-		<td class="td-center" colspan="3"><span class="b-nosize">&nbsp;</span></td>
+		<td class="td-center" colspan="2"><span class="b-nosize">&nbsp;</span></td>
 		<td class="td-right xspan b-nosize"><span class="span-right b-nosize">&nbsp;</span></td>
 	</tr>
 </table>
