d2e7fe264d03e9c06b1b6b790647a6e40a5aa1fc
Cedric Abunar <cabunar@avencall.com>
2013-02-04 15:32:53 -0500

    update contact page

--- src/tpl/www/bloc/xivo/contact.php
+++ src/tpl/www/bloc/xivo/contact.php
@@ -18,13 +18,12 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-	$url = $this->get_module('url');
+$url = $this->get_module('url');
 ?>
 <div class="b-infos">
 	<h3 class="sb-top xspan"><span class="span-left">&nbsp;</span><span class="span-center"><?=$this->bbf('title_content_name');?></span><span class="span-right">&nbsp;</span></h3>
 	<div class="sb-content">
-		<div class="logo"><?=$url->img_html('img/site/avencall.png',XIVO_AV_FR_CORP_NAME);?></div>
-		<div class="logo"><?=$url->img_html('img/site/proformatique.png',XIVO_PF_FR_CORP_NAME);?></div>
+		<div class="logo"><?=$url->img_html('img/site/logo_avencall_rgb_quadri_en.jpg',XIVO_AV_FR_CORP_NAME);?></div>
 		<div class="clear"></div>
 	</div>
 	<div class="sb-content">
@@ -43,37 +42,11 @@
 				<dd><?='<a href="http://'.XIVO_AV_FR_CORP_URL.'" title="'.XIVO_AV_FR_CORP_URL.'" target="_blank">'.XIVO_AV_FR_CORP_URL.'</a>'?></dd>
 				<dd><?='<a href="http://'.XIVO_SOFT_URL.'" title="'.XIVO_SOFT_LABEL.'" target="_blank">'.XIVO_SOFT_URL.'</a>'?></dd>
 		</dl>
-		<dl class="body">
-			<dt><?=$this->bbf('info_address');?></dt>
-				<dd><?=XIVO_PF_FR_CORP_ADDRESS?></dd>
-				<dd><?=XIVO_PF_FR_CORP_ZIPCODE?> <?=XIVO_PF_FR_CORP_CITY?></dd>
-				<dd><?=XIVO_PF_FR_CORP_COUNTRY?></dd>
-			<dt><?=$this->bbf('info_phone');?></dt>
-				<dd><?=XIVO_PF_FR_CORP_PHONE?></dd>
-			<dt><?=$this->bbf('info_fax');?></dt>
-				<dd><?=XIVO_PF_FR_CORP_FAX?></dd>
-			<dt><?=$this->bbf('info_e-mail');?></dt>
-				<dd><?='<a href="mailto:'.XIVO_PF_FR_CORP_EMAIL.'" title="'.XIVO_PF_FR_CORP_EMAIL.'">'.XIVO_PF_FR_CORP_EMAIL.'</a>'?></dd>
-			<dt><?=$this->bbf('info_websites');?></dt>
-				<dd><?='<a href="http://'.XIVO_PF_FR_CORP_URL.'" title="'.XIVO_PF_FR_CORP_LABEL.'" target="_blank">'.XIVO_PF_FR_CORP_URL.'</a>'?></dd>
-				<dd><?='<a href="http://'.XIVO_SOFT_URL.'" title="'.XIVO_SOFT_LABEL.'" target="_blank">'.XIVO_SOFT_URL.'</a>'?></dd>
-		</dl>
-		<dl class="body">
-			<dt><?=$this->bbf('info_address');?></dt>
-				<dd><?=XIVO_PF_CA_CORP_ADDRESS?></dd>
-				<dd><?=XIVO_PF_CA_CORP_ZIPCODE?> <?=XIVO_PF_CA_CORP_CITY?></dd>
-				<dd><?=XIVO_PF_CA_CORP_COUNTRY?></dd>
-			<dt><?=$this->bbf('info_phone');?></dt>
-				<dd><?=XIVO_PF_CA_CORP_PHONE?></dd>
-			<dt><?=$this->bbf('info_fax');?></dt>
-				<dd><?=XIVO_PF_CA_CORP_FAX?></dd>
-			<dt><?=$this->bbf('info_e-mail');?></dt>
-				<dd><?='<a href="mailto:'.XIVO_PF_CA_CORP_EMAIL.'" title="'.XIVO_PF_CA_CORP_EMAIL.'">'.XIVO_PF_CA_CORP_EMAIL.'</a>'?></dd>
-			<dt><?=$this->bbf('info_websites');?></dt>
-				<dd><?='<a href="http://'.XIVO_PF_CA_CORP_URL.'" title="'.XIVO_PF_CA_CORP_URL.'" target="_blank">'.XIVO_PF_CA_CORP_URL.'</a>'?></dd>
-				<dd><?='<a href="http://'.XIVO_SOFT_URL.'" title="'.XIVO_SOFT_LABEL.'" target="_blank">'.XIVO_SOFT_URL.'</a>'?></dd>
-		</dl>
 		<div class="clear"></div>
 	</div>
-	<div class="sb-foot xspan"><span class="span-left">&nbsp;</span><span class="span-center">&nbsp;</span><span class="span-right">&nbsp;</span></div>
+	<div class="sb-foot xspan">
+		<span class="span-left">&nbsp;</span>
+		<span class="span-center">&nbsp;</span>
+		<span class="span-right">&nbsp;</span>
+	</div>
 </div>
