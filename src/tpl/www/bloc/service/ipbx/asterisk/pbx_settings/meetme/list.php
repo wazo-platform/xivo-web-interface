03477c269298cef9848cd787781927b7ee41e7e7
Cedric Abunar <cabunar@avencall.com>
2014-05-28 13:57:58 -0400

    add entity column in meetme list page

--- src/tpl/www/bloc/service/ipbx/asterisk/pbx_settings/meetme/list.php
+++ src/tpl/www/bloc/service/ipbx/asterisk/pbx_settings/meetme/list.php
@@ -80,6 +80,7 @@ $page = $url->pager($pager['pages'],
 					$this->bbf('col_sort_confno'));
 ?>
 		</th>
+		<th class="th-center"><?=$this->bbf('col_entity');?></th>
 		<th class="th-center"><?=$this->bbf('col_pin');?></th>
 		<th class="th-center col-action"><?=$this->bbf('col_action');?></th>
 		<th class="th-right xspan"><span class="span-right">&nbsp;</span></th>
@@ -88,7 +89,7 @@ $page = $url->pager($pager['pages'],
 	if(($list = $this->get_var('list')) === false || ($nb = count($list)) === 0):
 ?>
 	<tr class="sb-content">
-		<td colspan="6" class="td-single"><?=$this->bbf('no_meetme');?></td>
+		<td colspan="7" class="td-single"><?=$this->bbf('no_meetme');?></td>
 	</tr>
 <?php
 	else:
@@ -122,6 +123,7 @@ $page = $url->pager($pager['pages'],
 			</label>
 		</td>
 		<td><?=(dwho_has_len($ref['confno']) === true ? $ref['confno'] : '-')?></td>
+		<td class="col_entity"><?=$ref['entity_displayname']?></td>
 		<td><?=(dwho_has_len($ref['pin']) === true ? $ref['pin'] : '-')?></td>
 		<td class="td-right" colspan="2">
 <?php
@@ -151,7 +153,7 @@ $page = $url->pager($pager['pages'],
 ?>
 	<tr class="sb-foot">
 		<td class="td-left xspan b-nosize"><span class="span-left b-nosize">&nbsp;</span></td>
-		<td class="td-center" colspan="4"><span class="b-nosize">&nbsp;</span></td>
+		<td class="td-center" colspan="5"><span class="b-nosize">&nbsp;</span></td>
 		<td class="td-right xspan b-nosize"><span class="span-right b-nosize">&nbsp;</span></td>
 	</tr>
 </table>
