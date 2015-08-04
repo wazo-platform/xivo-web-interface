4710de006d50e55f65dc10e6e198d77589bada86
Gregory Eric Sanderson <gsanderson@avencall.com>
2015-08-04 11:45:16 -0400

    add instrospection to confd mock
    will be used for checking what requests were sent to the mock
    and send prepared responses

--- integration_tests/assets/funckeys/mocks/confd/confd.py
+++ integration_tests/assets/funckeys/mocks/confd/confd.py
@@ -1,6 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
+
+REQUESTS = []
+
+RESPONSES = {}
 
 PROFILES = [{u'id': 1,
              u'links': [{u'href': u'https://webi:9486/1.1/cti_profiles/1',
@@ -20,6 +24,45 @@ PROFILES = [{u'id': 1,
              u'name': u'Client'}]
 
 
+@app.before_request
+def log_request():
+    if not (request.path.startswith('/_requests') or
+            request.path.startswith('/_responses')):
+        log = {'method': request.method,
+               'path': request.path,
+               'query': dict(request.args),
+               'body': request.data,
+               'headers': dict(request.headers)}
+        REQUESTS.append(log)
+
+
+@app.route('/_requests', methods=['GET'])
+def list_requests():
+    return jsonify(requests=REQUESTS)
+
+
+@app.route('/_requests', methods=['DELETE'])
+def delete_requests():
+    global REQUESTS
+    REQUESTS = []
+    return ''
+
+
+@app.route('/_responses', methods=['POST'])
+def add_response():
+    response = request.get_json(force=True)
+    method = RESPONSES.setdefault(request.method, {})
+    method[response['path']] = response['body']
+    return ''
+
+
+@app.route('/_responses', methods=['DELETE'])
+def delete_responses():
+    global RESPONSES
+    RESPONSES = {}
+    return ''
+
+
 @app.route('/1.1/devices')
 def devices():
     return jsonify(total=0,
@@ -33,9 +76,11 @@ def cti_profiles():
 
 
 @app.route('/1.1/<path:path>', methods=['GET', 'POST', 'PUT', 'DELETE'])
-def fallback(path):
-    return jsonify()
+def respond(path):
+    method = RESPONSES.get(request.method, {})
+    response = method.get(request.path, {})
+    return jsonify(**response)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=9487)
+    app.run(host="0.0.0.0", port=9487, debug=True)
