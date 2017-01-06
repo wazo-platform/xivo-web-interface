446f14096b198e2961f7debda7c7fd9538fae51d
Sebastien Duthil <dev.wazo+git@gmail.com>
2017-01-06 15:57:24 -0500

    fix whitespace

--- src/action/www/service/ipbx/asterisk/system_management/configfiles.php
+++ src/action/www/service/ipbx/asterisk/system_management/configfiles.php
@@ -2,7 +2,7 @@
 
 #
 # XiVO Web-Interface
-# Copyright (C) 2006-2014  Avencall
+# Copyright (C) 2006-2017 The Wazo Authors  (see the AUTHORS file)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,7 +29,7 @@ $configfiles = &$ipbx->get_module('configfiles');
 
 switch($act)
 {
-	case 'add':	
+	case 'add':
 		$info = array('configfile' => null);
 
 		if(isset($_QR['fm_send'], $_QR['configfile']['content']) === true)
