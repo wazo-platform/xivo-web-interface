bc08efd8c8c53eb0bfbe26a94292f88db7db5ad7
Sebastien Duthil <sduthil@wazo.community>
2017-04-14 15:29:46 -0400

    remove unused test code

--- integration_tests/suite/lib/provd.py
+++ integration_tests/suite/lib/provd.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+# -*- coding: utf-8 -*-
 
 # Copyright 2015-2017 The Wazo Authors  (see the AUTHORS file)
 #
@@ -15,12 +15,6 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-
-from datetime import datetime
-
-from docker import APIClient as DockerClient
-from hamcrest import assert_that, equal_to, has_item, starts_with
-from xivo_provd_client import NotFoundError
 
 
 class ProvdHelper(object):
@@ -99,34 +93,3 @@ class ProvdHelper(object):
     def add_default_configs(self):
         for config in self.DEFAULT_CONFIGS:
             self.configs.add(config)
-
-    def assert_config_does_not_exist(self, config_id):
-        try:
-            self.configs.get(config_id)
-        except NotFoundError:
-            return
-        else:
-            raise AssertionError('config "{}" exists in xivo-provd'.format(config_id))
-
-    def assert_device_has_autoprov_config(self, device):
-        assert_that(device[u'config'], starts_with(u'autoprov'))
-
-    def assert_config_use_device_template(self, config, template_id):
-        assert_that(config[u'configdevice'], equal_to(template_id))
-        assert_that(config[u'parent_ids'], has_item(template_id))
-
-    def has_synchronized(self, device_id, timestamp=None):
-        timestamp = timestamp or datetime.utcnow()
-        line = "Synchronizing device {}".format(device_id)
-        output = self.find_provd_logs(timestamp)
-        for log in output.split("\n"):
-            if line in log:
-                return True
-        return False
-
-    def find_provd_logs(self, timestamp):
-        client = DockerClient()
-        for container in client.containers(filters={'status': 'running'}):
-            info = client.inspect_container(container['Id'])
-            if info['Config']['Image'] == self.DOCKER_PROVD_IMAGE:
-                return client.logs(container['Id'], since=timestamp)
