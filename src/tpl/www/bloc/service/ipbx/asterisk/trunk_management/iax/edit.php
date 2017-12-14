980190336fbca2284517174d1f7fbdc81043918a
Jean-Yves LEBLEU <jylebleu@avencall.com>
2017-12-14 16:36:07 +0100

    1116 iax trunk submenu

--- src/tpl/www/bloc/service/ipbx/asterisk/trunk_management/iax/edit.php
+++ src/tpl/www/bloc/service/ipbx/asterisk/trunk_management/iax/edit.php
@@ -2,7 +2,7 @@
 
 #
 # XiVO Web-Interface
-# Copyright (C) 2006-2014  Avencall
+# Copyright (C) 2006-2017  Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,12 +29,8 @@ $info = $this->get_var('info');
 		<span class="span-right">&nbsp;</span>
 	</h3>
 
-<?php
-	$this->file_include('bloc/service/ipbx/asterisk/trunk_management/iax/submenu');
-?>
-
 	<div class="sb-content">
-		<form action="#" method="post" accept-charset="utf-8" onsubmit="dwho.form.select('it-codec');">
+		<form class="form-horizontal" action="#" method="post" accept-charset="utf-8" onsubmit="dwho.form.select('it-codec');">
 <?php
 		echo	$form->hidden(array('name'	=> DWHO_SESS_NAME,
 					    'value'	=> DWHO_SESS_ID)),
