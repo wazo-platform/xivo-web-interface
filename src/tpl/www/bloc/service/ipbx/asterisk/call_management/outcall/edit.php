9acd4f2b4d6dbd3ce8b2b9e8554b7e63f7921e03
Jean-Yves LEBLEU <jylebleu@avencall.com>
2017-12-14 18:34:15 +0100

    1116 outcall submenu

--- src/tpl/www/bloc/service/ipbx/asterisk/call_management/outcall/edit.php
+++ src/tpl/www/bloc/service/ipbx/asterisk/call_management/outcall/edit.php
@@ -2,7 +2,7 @@
 
 #
 # XiVO Web-Interface
-# Copyright (C) 2006-2014  Avencall
+# Copyright (C) 2006-2017  Avencall
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -29,19 +29,15 @@ $info = $this->get_var('info');
 		<span class="span-right">&nbsp;</span>
 	</h3>
 
-<?php
-	$this->file_include('bloc/service/ipbx/asterisk/call_management/outcall/submenu');
-?>
-
 	<div class="sb-content">
-<form action="#" method="post" accept-charset="utf-8">
-<?=$form->hidden(array('name' => DWHO_SESS_NAME,'value' => DWHO_SESS_ID));?>
-<?=$form->hidden(array('name' => 'fm_send','value' => 1));?>
-<?=$form->hidden(array('name' => 'act','value' => 'edit'));?>
-<?=$form->hidden(array('name' => 'id','value' => $this->get_var('id')));?>
-<?php $this->file_include('bloc/service/ipbx/asterisk/call_management/outcall/form');?>
-<?=$form->submit(array('name' => 'submit','id' => 'it-submit','value' => $this->bbf('fm_bt-save')));?>
-</form>
+		<form class="form-horizontal" action="#" method="post" accept-charset="utf-8">
+		<?=$form->hidden(array('name' => DWHO_SESS_NAME,'value' => DWHO_SESS_ID));?>
+		<?=$form->hidden(array('name' => 'fm_send','value' => 1));?>
+		<?=$form->hidden(array('name' => 'act','value' => 'edit'));?>
+		<?=$form->hidden(array('name' => 'id','value' => $this->get_var('id')));?>
+		<?php $this->file_include('bloc/service/ipbx/asterisk/call_management/outcall/form');?>
+		<?=$form->submit(array('name' => 'submit','id' => 'it-submit','value' => $this->bbf('fm_bt-save')));?>
+		</form>
 	</div>
 	<div class="sb-foot xspan"><span class="span-left">&nbsp;</span><span class="span-center">&nbsp;</span><span class="span-right">&nbsp;</span></div>
 </div>
