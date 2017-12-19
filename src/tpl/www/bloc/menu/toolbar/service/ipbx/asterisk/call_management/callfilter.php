8f1a4d9fa1dded803c3ee6866845a58fe334476a
Laurent Meiller <lmeiller@avencall.com>
2017-12-19 16:41:15 +0100

    1116 add toolbar for callfilter

--- src/tpl/www/bloc/menu/toolbar/service/ipbx/asterisk/call_management/callfilter.php
+++ src/tpl/www/bloc/menu/toolbar/service/ipbx/asterisk/call_management/callfilter.php
@@ -30,41 +30,7 @@ $dhtml->write_js($toolbar_js);
 
 ?>
 <script type="text/javascript" src="<?=$this->file_time($this->url('js/xivo_toolbar.js'));?>"></script>
-<?php
 
-echo	$url->href_html($url->img_html('img/menu/top/toolbar/bt-add.gif',
-				       $this->bbf('toolbar_opt_add'),
-				       'id="toolbar-bt-add"
-					border="0"'),
-			'service/ipbx/call_management/callfilter',
-			'act=add',
-			null,
-			$this->bbf('toolbar_opt_add'));
-
-if($this->get_var('act') === 'list'):
-	echo	$url->img_html('img/menu/top/toolbar/bt-more.gif',
-			       $this->bbf('toolbar_opt_advanced'),
-			       'id="toolbar-bt-advanced"
-				border="0"');
-?>
-<div class="sb-advanced-menu">
-	<ul id="toolbar-advanced-menu">
-		<li>
-			<a href="#" id="toolbar-advanced-menu-enable"><?=$this->bbf('toolbar_adv_menu_enable');?></a>
-		</li>
-		<li>
-			<a href="#" id="toolbar-advanced-menu-disable"><?=$this->bbf('toolbar_adv_menu_disable');?></a>
-		</li>
-		<li>
-			<a href="#" id="toolbar-advanced-menu-select-all"><?=$this->bbf('toolbar_adv_menu_select-all');?></a>
-		</li>
-		<li>
-			<a href="#" id="toolbar-advanced-menu-delete"><?=$this->bbf('toolbar_adv_menu_delete');?></a>
-		</li>
-	</ul>
-</div>
-<?php
-
-endif;
-
-?>
+<toolbar-buttons actions="['add']"
+	actions-adv="['toolbar-advanced-menu-enable','toolbar-advanced-menu-disable', 'toolbar-advanced-menu-select-all', 'toolbar-advanced-menu-delete']"
+	display-adv-on="list"></toolbar-buttons>
