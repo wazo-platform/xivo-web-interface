cfa0c9a341fcccb8f9a728c575fecc7cad6e5291
Jean-Yves LEBLEU <jylebleu@avencall.com>
2017-12-20 08:39:07 +0100

    1116 cc queue announce search and list

--- src/www/js/xivo/app.js
+++ src/www/js/xivo/app.js
@@ -8,6 +8,7 @@ import run from './xivo.run';
 /*Module dependencies */
 import IpbxUsersController from './controllers/ipbx/users.controllers';
 import CCAgentsController from './controllers/callcenter/agents.controllers';
+import CCQueuesController from './controllers/callcenter/queues.controllers';
 import toolbar from './services/toolbar.service';
 import toolbarButtons from './directives/toolbarButtons.directive';
 import toolbarSearch from './directives/toolbarSearch.directive';
@@ -19,6 +20,7 @@ angular.module('Xivo', [angulartranslate, uibootstrap])
 .service('toolbar',toolbar)
 .controller('IpbxUsersController', IpbxUsersController)
 .controller('CCAgentsController', CCAgentsController)
+.controller('CCQueuesController', CCQueuesController)
 .directive('toolbarButtons',toolbarButtons)
 .directive('toolbarSearch',toolbarSearch)
 .directive('onFinishRender',onFinishRender)
