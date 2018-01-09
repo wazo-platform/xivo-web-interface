3d68e27feddb9b3235ce35c6197f4036b8cffb98
Jean-Yves LEBLEU <jylebleu@avencall.com>
2018-01-09 16:07:48 +0100

    1640 breadcrumb on support xivo

--- src/tpl/www/bloc/xivo/configuration/support/xivo.php
+++ src/tpl/www/bloc/xivo/configuration/support/xivo.php
@@ -27,11 +27,9 @@ $element = $this->get_var('element');
 ?>
 
 <div class="b-infos b-form">
-<h3 class="sb-top xspan">
-	<span class="span-left">&nbsp;</span>
-	<span class="span-center"><?=$this->bbf('title_content_name');?></span>
-	<span class="span-right">&nbsp;</span>
-</h3>
+<breadcrumb
+        page="<?=$this->bbf('title_content_name');?>">
+</breadcrumb>
 
 <div class="sb-content">
 <form action="#" method="post" accept-charset="utf-8">
@@ -60,9 +58,5 @@ echo	$form->submit(array('name'	=> 'submit',
 ?>
 </form>
 	</div>
-	<div class="sb-foot xspan">
-		<span class="span-left">&nbsp;</span>
-		<span class="span-center">&nbsp;</span>
-		<span class="span-right">&nbsp;</span>
-	</div>
+	<div class="sb-foot xspan"></div>
 </div>
