f487cd7b85f9cda1f100516c864a19439b9cb578
Laurent Meiller <lmeiller@avencall.com>
2018-01-09 17:46:18 +0100

    1640 add breadcrumb for stats

--- src/tpl/www/bloc/statistics/index.php
+++ src/tpl/www/bloc/statistics/index.php
@@ -22,17 +22,12 @@ $url     = &$this->get_module('url');
 
 ?>
 <div class="b-infos b-form">
-	<h3 class="sb-top xspan">
-		<span class="span-left">&nbsp;</span>
-		<span class="span-center"><?=$this->bbf('title_content_name');?></span>
-		<span class="span-right">&nbsp;</span>
-	</h3>
+	<breadcrumb
+		page="<?=$this->bbf('title_content_name');?>">
+	</breadcrumb>
+
 	<div class="sb-content">
 		HOME_STATS
     </div>
-	<div class="sb-foot xspan">
-		<span class="span-left">&nbsp;</span>
-		<span class="span-center">&nbsp;</span>
-		<span class="span-right">&nbsp;</span>
-	</div>
+	<div class="sb-foot xspan"></div>
 </div>
