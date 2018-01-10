996432cb1387537d7b0f99f357a228bebfa28266
Laurent Meiller <lmeiller@avencall.com>
2018-01-10 10:26:59 +0100

    1640 add breadcrumb for dhcp

--- src/tpl/www/bloc/xivo/configuration/network/dhcp.php
+++ src/tpl/www/bloc/xivo/configuration/network/dhcp.php
@@ -27,13 +27,12 @@ $netifaces = $this->get_var('netifaces');
 ?>
 
 <div class="b-infos b-form">
-	<h3 class="sb-top xspan">
-		<span class="span-left">&nbsp;</span> <span class="span-center"><?=$this->bbf('title_content_name');?>
-		</span> <span class="span-right">&nbsp;</span>
-	</h3>
+	<breadcrumb
+		page="<?=$this->bbf('title_content_name');?>">
+	</breadcrumb>
 
 	<div class="sb-content">
-		<form action="#" method="post" accept-charset="utf-8">
+		<form action="#" method="post" accept-charset="utf-8" class="form-horizontal">
 
 			<div id="sb-part-first">
 				<?php
