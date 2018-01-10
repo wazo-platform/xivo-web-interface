058687761819d617893c91d773b713bd4c3de73b
Jean-Yves LEBLEU <jylebleu@avencall.com>
2018-01-10 09:01:56 +0100

    1640 breadcrumb dns resolver

--- src/tpl/www/bloc/xivo/configuration/network/resolvconf.php
+++ src/tpl/www/bloc/xivo/configuration/network/resolvconf.php
@@ -25,11 +25,9 @@ $element = $this->get_var('element');
 
 ?>
 <div class="b-infos b-form">
-<h3 class="sb-top xspan">
-	<span class="span-left">&nbsp;</span>
-	<span class="span-center"><?=$this->bbf('title_content_name');?></span>
-	<span class="span-right">&nbsp;</span>
-</h3>
+<breadcrumb
+        page="<?=$this->bbf('title_content_name');?>">
+</breadcrumb>
 
 <div class="sb-content">
 <form class="form-horizontal" action="#" method="post" accept-charset="utf-8">
